2ba589589d4dbc34b72e23bfbc9a4dd4b71c6c1a
StarkMP <stark.maks.pacs@gmail.com>
2026-03-20 17:16:00 +0300

    chore(build): update vsDevShellPath in build script

--- BuildRelease.ps1
+++ BuildRelease.ps1
@@ -3,14 +3,13 @@ param(
     [int]$threads = 8
 )
 
-$vsDevShellPath = "C:/Program Files/Microsoft Visual Studio/2022/Professional/Common7/Tools/Launch-VsDevShell.ps1"
+$vsDevShellPath = "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/Launch-VsDevShell.ps1"
 
 # Load in local variable overrides
 # local developer copy of Build_Config_Template.ps1 to override $vsDevShellPath or other variables if needed
 if (Test-Path .\Build_Config_Local.ps1) {
     . .\Build_Config_Local.ps1
 }
-
 
 # Save current directory, launch VS dev shell, and return to original directory
 $currentDirectory = $PWD.Path

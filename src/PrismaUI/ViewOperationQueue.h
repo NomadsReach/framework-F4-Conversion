efc660e032dded6613f16c9d648501f31245d071
Nomads_Reach <144523850+NomadsReach@users.noreply.github.com>
2026-08-17 15:52:30 -0400

    Clean operation queue declarations

--- src/PrismaUI/ViewOperationQueue.h
+++ src/PrismaUI/ViewOperationQueue.h
@@ -1,14 +1,9 @@
 #pragma once
 
-#include <atomic>
 #include <functional>
-#include <memory>
-#include <mutex>
-#include <queue>
 
 namespace PrismaUI::Core {
     typedef uint64_t PrismaViewId;
-    struct PrismaView;
 }
 
 namespace PrismaUI::ViewOperationQueue {
@@ -17,14 +12,9 @@ namespace PrismaUI::ViewOperationQueue {
     constexpr size_t MAX_OPERATIONS_PER_VIEW = 100;
 
     bool EnqueueOperation(Core::PrismaViewId viewId, OperationFunc operation);
-
     void ProcessNextOperation(Core::PrismaViewId viewId);
-
     void ProcessAllViewOperations();
-
     void ClearOperations(Core::PrismaViewId viewId);
-
     size_t GetQueueSize(Core::PrismaViewId viewId);
-
     bool IsProcessing(Core::PrismaViewId viewId);
 }

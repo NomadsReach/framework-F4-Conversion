ec08e589decbb07c713f2b1d8e86a9ec68932ee2
Nomads_Reach <144523850+NomadsReach@users.noreply.github.com>
2026-08-17 15:43:28 -0400

    Normalize API implementation declarations

--- src/API/API.h
+++ src/API/API.h
@@ -2,65 +2,55 @@
 
 #include "PrismaUI_F4_API.h"
 
-class PluginAPI
-{
-	using LatestInterface = PRISMA_UI_API::IVPrismaUI4;
+class PluginAPI {
+    using LatestInterface = PRISMA_UI_API::IVPrismaUI4;
 
 public:
-	class PrismaUIInterface : public LatestInterface
-	{
-	private:
-		PrismaUIInterface() noexcept {};
-		virtual ~PrismaUIInterface() noexcept {};
+    class PrismaUIInterface : public LatestInterface {
+    private:
+        PrismaUIInterface() noexcept {}
+        virtual ~PrismaUIInterface() noexcept {}
 
-	public:
-		static PrismaUIInterface* GetSingleton() noexcept
-		{
-			static PrismaUIInterface singleton;
-			return std::addressof(singleton);
-		}
+    public:
+        static PrismaUIInterface* GetSingleton() noexcept {
+            static PrismaUIInterface singleton;
+            return std::addressof(singleton);
+        }
 
-		// IVPrismaUI1 (order needs to match PrismaUI_API.h exactly for correct vtable layout)
+        // Vtable order must match PrismaUI_F4_API.h.
+        virtual PrismaView CreateView(
+            const char* htmlPath, PRISMA_UI_API::OnDomReadyCallback onDomReadyCallback = nullptr) noexcept override;
+        virtual void Invoke(PrismaView view, const char* script,
+                            PRISMA_UI_API::JSCallback callback = nullptr) noexcept override;
+        virtual void InteropCall(PrismaView view, const char* functionName, const char* argument) noexcept override;
+        virtual void RegisterJSListener(PrismaView view, const char* fnName,
+                                        PRISMA_UI_API::JSListenerCallback callback) noexcept override;
+        virtual bool HasFocus(PrismaView view) noexcept override;
+        virtual bool Focus(PrismaView view, bool pauseGame = false, bool disableFocusMenu = false) noexcept override;
+        virtual void Unfocus(PrismaView view) noexcept override;
+        virtual void Show(PrismaView view) noexcept override;
+        virtual void Hide(PrismaView view) noexcept override;
+        virtual bool IsHidden(PrismaView view) noexcept override;
+        virtual int GetScrollingPixelSize(PrismaView view) noexcept override;
+        virtual void SetScrollingPixelSize(PrismaView view, int pixelSize) noexcept override;
+        virtual bool IsValid(PrismaView view) noexcept override;
+        virtual void Destroy(PrismaView view) noexcept override;
+        virtual void SetOrder(PrismaView view, int order) noexcept override;
+        virtual int GetOrder(PrismaView view) noexcept override;
+        virtual void CreateInspectorView(PrismaView view) noexcept override;
+        virtual void SetInspectorVisibility(PrismaView view, bool visible) noexcept override;
+        virtual bool IsInspectorVisible(PrismaView view) noexcept override;
+        virtual void SetInspectorBounds(PrismaView view, float topLeftX, float topLeftY, unsigned int width,
+                                        unsigned int height) noexcept override;
+        virtual bool HasAnyActiveFocus() noexcept override;
+        virtual void RegisterConsoleCallback(PrismaView view,
+                                             PRISMA_UI_API::ConsoleMessageCallback callback) noexcept override;
+        virtual void RegisterTranslations(PrismaView view, const char* pluginName) noexcept override;
+        virtual void BindUIEvent(PrismaView view, const char* functionName,
+                                 PRISMA_UI_API::JSListenerCallback callback) noexcept override;
+        virtual void EnumerateViews(PRISMA_UI_API::ViewEnumCallback callback, void* userdata) noexcept override;
 
-		virtual PrismaView CreateView(const char* htmlPath, PRISMA_UI_API::OnDomReadyCallback onDomReadyCallback = nullptr) noexcept override;
-		virtual void Invoke(PrismaView view, const char* script, PRISMA_UI_API::JSCallback callback = nullptr) noexcept override;
-		virtual void InteropCall(PrismaView view, const char* functionName, const char* argument) noexcept override;
-		virtual void RegisterJSListener(PrismaView view, const char* fnName, PRISMA_UI_API::JSListenerCallback callback) noexcept override;
-		virtual bool HasFocus(PrismaView view) noexcept override;
-		virtual bool Focus(PrismaView view, bool pauseGame = false, bool disableFocusMenu = false) noexcept override;
-		virtual void Unfocus(PrismaView view) noexcept override;
-		virtual void Show(PrismaView view) noexcept override;
-		virtual void Hide(PrismaView view) noexcept override;
-		virtual bool IsHidden(PrismaView view) noexcept override;
-		virtual int GetScrollingPixelSize(PrismaView view) noexcept override;
-		virtual void SetScrollingPixelSize(PrismaView view, int pixelSize) noexcept override;
-		virtual bool IsValid(PrismaView view) noexcept override;
-		virtual void Destroy(PrismaView view) noexcept override;
-		virtual void SetOrder(PrismaView view, int order) noexcept override;
-		virtual int GetOrder(PrismaView view) noexcept override;
-		virtual void CreateInspectorView(PrismaView view) noexcept override;
-		virtual void SetInspectorVisibility(PrismaView view, bool visible) noexcept override;
-		virtual bool IsInspectorVisible(PrismaView view) noexcept override;
-		virtual void SetInspectorBounds(PrismaView view, float topLeftX, float topLeftY, unsigned int width, unsigned int height) noexcept override;
-		virtual bool HasAnyActiveFocus() noexcept override;
-
-		// IVPrismaUI2
-
-		virtual void RegisterConsoleCallback(PrismaView view, PRISMA_UI_API::ConsoleMessageCallback callback) noexcept override;
-
-		// IVPrismaUI3
-
-		virtual void RegisterTranslations(PrismaView view, const char* pluginName) noexcept override;
-
-		// IVPrismaUI4
-
-		virtual void BindUIEvent(PrismaView view, const char* functionName,
-		                         PRISMA_UI_API::JSListenerCallback callback) noexcept override;
-
-		virtual void EnumerateViews(PRISMA_UI_API::ViewEnumCallback callback,
-		                            void* userdata) noexcept override;
-
-	private:
-		unsigned long apiTID = 0;
-	};
+    private:
+        unsigned long apiTID = 0;
+    };
 };

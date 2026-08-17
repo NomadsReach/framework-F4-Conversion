a3c99a759ae219897814a98552482cc7812b520f
Nomads_Reach <144523850+NomadsReach@users.noreply.github.com>
2026-08-17 15:53:11 -0400

    Normalize D3D hook declarations

--- src/Hooks/Hooks.h
+++ src/Hooks/Hooks.h
@@ -5,16 +5,17 @@
 
 namespace Hooks {
     struct D3DHooks {
-        using PresentFunc       = HRESULT(APIENTRY*)(REX::W32::IDXGISwapChain*, UINT, UINT);
-        using ResizeBuffersFunc = HRESULT(APIENTRY*)(REX::W32::IDXGISwapChain*, UINT, UINT, UINT, REX::W32::DXGI_FORMAT, UINT);
+        using PresentFunc = HRESULT(APIENTRY*)(REX::W32::IDXGISwapChain*, UINT, UINT);
+        using ResizeBuffersFunc =
+            HRESULT(APIENTRY*)(REX::W32::IDXGISwapChain*, UINT, UINT, UINT, REX::W32::DXGI_FORMAT, UINT);
 
-        static inline PresentFunc       oPresent       = nullptr;
+        static inline PresentFunc oPresent = nullptr;
         static inline ResizeBuffersFunc oResizeBuffers = nullptr;
 
-        static HRESULT APIENTRY HookPresent(REX::W32::IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
-        static HRESULT APIENTRY HookResizeBuffers(REX::W32::IDXGISwapChain* pSwapChain, UINT BufferCount,
-                                                   UINT Width, UINT Height,
-                                                   REX::W32::DXGI_FORMAT NewFormat, UINT SwapChainFlags);
+        static HRESULT APIENTRY HookPresent(REX::W32::IDXGISwapChain* pSwapChain, UINT syncInterval, UINT flags);
+        static HRESULT APIENTRY HookResizeBuffers(REX::W32::IDXGISwapChain* pSwapChain, UINT bufferCount, UINT width,
+                                                  UINT height, REX::W32::DXGI_FORMAT newFormat,
+                                                  UINT swapChainFlags);
 
         static void Install();
         static void Uninstall();
